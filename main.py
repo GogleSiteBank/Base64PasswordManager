6f0757da67664ebaf660f30b5d788a4efbd2a3c5
Connor <125816677+GogleSiteBank@users.noreply.github.com>
2023-05-13 16:55:27 +0100

    Update main.py
    Massive update, much harder to crack now + Smaller script

--- main.py
+++ main.py
@@ -1,67 +1,33 @@
-import base64
-import json
-import random
-import string
+import base64, random, string
 
-f  = open('manager.json', 'w')
+def generateFileName():
+    return "output" + "".join(random.choices(string.ascii_letters, k=12)) + ".txt"
+f = open(generateFileName(), "a")
+print("File will be saved to \"%s\"" % f.name)
 
-stri = string.ascii_letters + string.digits
-strs = string.ascii_letters + string.digits + string.punctuation + string.octdigits + string. hexdigits
-digi = string.digits
-amnts = 2,3
-z = random.choice(amnts)
-passa = "".join(random.choices(strs, k=25))
-passw = passa.encode('utf-8')
-rand = ''.join(random.choices(stri, k=25))
-key = ""
-def getKey():
-    global key
-    key = "".join(random.choices(strs, k=25))
-getKey()
 
-def encode():
-    inp = str(input("Code ran successfully!\nEnter the passwords, emails, etc: ")).encode('utf-8')
-    return inp
+def getRandomstring():
+    randomint = random.randint(1, 50)
+    return "".join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=randomint))
 
-def decode():
-    file = open('manager.json', 'r')
-    read = file.read()
-    r = base64.standard_b64decode(read)
-    f = open("returned.txt", "w")
-    f.write(r.decode('utf-8'))
+stopInt = random.randint(0,998)
 
-digits = string.digits
-randig = int(''.join(random.choices(digits, k=2)))
-for i in range(999):
-    k = int("".join(random.choices(digi, k=z)))
-    randig -= 1
-    if i != randig:
-        if i != 998:
-            a = string.ascii_letters + string.digits + string.punctuation + string.octdigits + string. hexdigits
-            b = "".join(random.choices(a, k=k))
-            b6 = base64.b64encode(b.encode('utf-8')).decode('utf-8')
-            a1 = string.ascii_letters + string.digits + string.punctuation + string.octdigits + string. hexdigits
-            b1 = "".join(random.choices(a, k=k))
-            b1_bytes = b1.encode('utf-8')
-            b61 = base64.standard_b64encode(b1_bytes).decode('utf-8')
-            f.write(json.dumps({f"{b6}": f"{b61}"})  + ",\n")
+
+def getPasswords():
+    return base64.b64encode((input("Passwords, emails, etc: ").encode('utf-8'))).decode('utf-8')
+
+def main():
+    print("Writing lines...")
+    for i in range(999):
+        if i == stopInt:
+            key = getRandomstring()
+            f.write(key)
+            f.write(getPasswords())
+            endKey = getRandomstring()
+            f.write(endKey)
+            print("Your First Key:\n%s\nYour second Key:\n%s" % (key, endKey))
         else:
-            a = string.ascii_letters + string.digits + string.punctuation + string.octdigits + string. hexdigits
-            b = "".join(random.choices(a, k=k))
-            b6 = base64.b64encode(b.encode('utf-8')).decode('utf-8')
-            a1 = string.ascii_letters + string.digits + string.punctuation + string.octdigits + string. hexdigits
-            b1 = "".join(random.choices(a, k=k))
-            b1_bytes = b1.encode('utf-8')
-            b61 = base64.standard_b64encode(b1_bytes).decode('utf-8')
-            f.write(json.dumps({f"{b6}==": f"{b61}"}) )
-    else:
-        if i != 997:
-            b64 = base64.standard_b64encode(encode()).decode('utf-8')
-            f.write(json.dumps({f"{b64}": f"{key}"}) + ",\n")
-        else:
-            b64 = base64.standard_b64encode(encode()).decode('utf-8')
-            f.write(json.dumps({f"{b64}": f"{key}"}))
-
-print(f"Your key is {key}")
-decode()
-f.close()
+            f.write(getRandomstring())
+    
+if __name__ == "__main__":
+    main()
